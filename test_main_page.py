6551cc6d5ff9d0f813d56efe6be6dfc0887f03b0
Vasily Popov <vasily.popov.me@gmail.com>
2020-04-17 22:21:41 +0200

    extract code to help method

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,9 @@
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
 
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link)
+    go_to_login_page(browser)
