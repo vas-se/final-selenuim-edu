85374adbd1aad6aeeb2f9654cd630c48b8338048
Vasily Popov <vasily.popov.me@gmail.com>
2020-04-18 02:36:10 +0200

    Add Parametrize link, find broken link and mark it as failed

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,7 +4,7 @@ import math
 
 
 class BasePage:
-    def __init__(self, browser, url, timeout=10):
+    def __init__(self, browser, url, timeout=20):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
